aff92bb36660cb02f3cf1415c0a75e9dd07f115f
Arnaud Bienvenu <abi@abienvenu.net>
2019-08-10 14:08:39 +0200

    pas besoin de config

--- action-calculatrice.py
+++ action-calculatrice.py
@@ -1,34 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import configparser
-import io
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
-
-
-class SnipsConfigParser(configparser.SafeConfigParser):
-    def to_dict(self):
-        return {
-            section: {
-                option_name: option
-                for option_name, option in self.items(section)
-            }
-            for section in self.sections()
-        }
-
-
-def read_configuration_file():
-    try:
-        with io.open(
-            "config.ini",
-            encoding="utf-8"
-        ) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, configparser.Error):
-        return dict()
 
 
 def humaniser(nombre):
